3d09c1bcc7ecf5b5e5c19c4970587d5f43874d66
Vincent Cantin <vincent.cantin@le-moulin-studio.com>
2013-02-09 01:05:17 +0800

    Empty working map.

--- src/com/lemoulinstudio/bikefriend/MainActivity.java
+++ src/com/lemoulinstudio/bikefriend/MainActivity.java
@@ -1,16 +1,36 @@
 package com.lemoulinstudio.bikefriend;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
-  /**
-   * Called when the activity is first created.
-   */
+  private GoogleMap map;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    setContentView(R.layout.station_map);
   }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    setupMap();
+  }
+
+  private void setupMap() {
+    if (map == null) {
+      map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.station_map)).getMap();
+      
+      if (map != null) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+      }
+    }
+  }
+
 }
